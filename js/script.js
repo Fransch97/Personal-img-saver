b5148244507086bf54df590e65432a8970d680db
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-09 20:37:16 +0200

    test

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("work");
+const body = document.querySelector('body');
+console.log(body);
+const saver = document.querySelector('.save');
+console.log('save');
+
+saver.addEventListener('click', function(){
+console.log("button works");
+})
