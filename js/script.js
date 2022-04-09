c548af15cca1b9db0daf58f5f0af7d97b3f4ede3
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-09 21:50:03 +0200

    img saver

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 console.log("work");
-const body = document.querySelector('body');
-console.log(body);
-const saver = document.querySelector('.save');
+let saver = document.querySelector('.save');
 console.log('save');
 
 saver.addEventListener('click', function(){
+let imgBank = document.querySelector('.imgbank');
+console.log(imgBank);
+
 console.log("button works");
+for (i = 0; i<1; i++){
+    let imgLink = document.querySelector('input').value;
+    console.log(imgLink);
+    let savedImg = `<img src="${imgLink}" alt="">`;
+    console.log(savedImg);
+    imgBank.innerHTML += savedImg;
+    document.querySelector('input').value = '';
+}
 })
