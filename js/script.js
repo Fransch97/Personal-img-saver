4224ca6d851ab0d66a5c7c5cae9c9621a98c06bf
fransch97 <fransch345@gmail.com>
2022-04-10 13:36:49 +0200

    deleted remove bug

--- js/script.js
+++ js/script.js
@@ -16,29 +16,38 @@ console.log(imgCounter)
     if(imgCounter <= 10){
         let imgLink = document.querySelector('input').value;
         console.log(imgLink);
-        let savedImg = `<div class="addedimg${imgCounter}" style="display:inline-block;"><img src="${imgLink}" alt=""> <button class="rimuovi${imgCounter}">Rimuovi</button></div>`;
+        let savedImg = `<div class="addedimg" style="display:inline-block;"><img src="${imgLink}" alt=""> <button onclick="this.parentElement.remove();" class="rimuovi">Rimuovi</button></div>`;
         console.log(savedImg);
         imgBank.innerHTML += savedImg;
         document.querySelector('input').value = '';
     }else{
         alert("Hai già inserito il massimo")
     }
+// let scrpitToAdd = document.querySelector('.extrascrpits');
+// console.log(scrpitToAdd);
+// scrpitToAdd.innerHTML += `
+// let divToRemove${imgCounter} = document.querySelector('.addedimg${imgCounter}');
+// let rimuovifunction${imgCounter} = document.querySelector('.rimuovi${imgCounter}');
+// rimuovifunction${imgCounter}.addEventListener('click', function(){
+//     console.log("funziona");
+//     divToRemove${imgCounter}.remove();
+// })
+// `
+;
+// let divToRemove = document.querySelector('.addedimg');
+// let rimuovifunction = document.querySelector('.rimuovi');
+//     rimuovifunction.addEventListener('click', function(){
+//         console.log("funziona");
+//         console.log(rimuovifunction.target)
+        // rimuovifunction.target.remove();
+// })
 
-let divToRemove = document.querySelector('.addedimg'+ imgCounter);
-   
-let rimuovifunction = document.querySelector('.rimuovi'+imgCounter);
-    rimuovifunction.addEventListener('click', function(){
-        console.log("funziona");
-        divToRemove.remove();
+})
+
+
+
         
-        divToRemove = imgCounter--;
-        rimuovifunction = imgCounter--;
-})
-
-
-})
-
-
+        
 
     
 
